6054771ec85f60471f1559543fe34d2ae9cede04
Kelly Feke <krf9453@rit.edu>
2023-06-08 15:40:06 -0400

    sneasy runs through ciam with param and output

--- mimiCIAM_demo/run_ciam_data_function.jl
+++ mimiCIAM_demo/run_ciam_data_function.jl
@@ -4,6 +4,7 @@ using PlotlyJS
 using NetCDF
 using CSV
 using DataFrames
+
 #=
 Combined function that gets npv, optimal, and no adapt cost in same loop 
 @param model = given ciam model
@@ -11,14 +12,20 @@ Combined function that gets npv, optimal, and no adapt cost in same loop
 @param lslr = local sea level rise data for segments
 @param segments = list of segments
 @param trials = number of trials for lslr
+@param ts = number of timesteps
+@param at = at parameter 
+@param params = list of parameter to test
+@num_params = number of parameter trials 
 @return tuple matrices for optimal cost 2050 and 2100, no adapt cost 2050 and 2100, net present value, and optimal strategy
 =#
 
-function run_ciam_data_f(model, ssprcp, lslr, segments, trials,ts, at)
+function run_ciam_data_f(model, ssprcp, lslr, segments, trials,ts, at, params = nothing, num_params = 1)
 
     # op_array/na_array = rows are segments, columns are models/trial
     #npv_array = rows are segments, columns are models/trials 
     #optimal_adapt = rows are segemnts, columns are models/trials
+
+    #if params are tested, order of cols in matrix is param 1 trial 1, param 2 trial 1, etc
     op_array_2050 = []
     op_array_2100 = []
     na_array_2050 = []
@@ -30,7 +37,6 @@ function run_ciam_data_f(model, ssprcp, lslr, segments, trials,ts, at)
     update_param!(model, :slrcost, :ntsteps, ts)
     update_param!(model, :slrcost, :at, at)
     MimiCIAM.run(model)
-
     
     
         for i in range(1,trials)
@@ -40,66 +46,117 @@ function run_ciam_data_f(model, ssprcp, lslr, segments, trials,ts, at)
             end
             update_param!(model, :slrcost, :lslr, lslr_trial)
             run(model)
-            optadapt_temp = []
-            for k in range(1, length(segments))
-                op_cost = model[:slrcost, :OptimalCost][1,k]
-                if op_cost == model[:slrcost, :ProtectCost][:,:,1][1,k]
-                    push!(optadapt_temp, "Protect-1")
-                elseif op_cost == model[:slrcost, :ProtectCost][:,:,2][1,k]
-                    push!(optadapt_temp, "Protect-2")
-                elseif op_cost == model[:slrcost, :ProtectCost][:,:,3][1,k]
-                    push!(optadapt_temp, "Protect-3")
-                elseif op_cost == model[:slrcost, :ProtectCost][:,:,4][1,k]
-                    push!(optadapt_temp, "Protect-4")
-                elseif op_cost == model[:slrcost, :ProtectCost][:,:,5][1,k]
-                    push!(optadapt_temp, "Protect-5")
-                elseif op_cost == model[:slrcost, :RetreatCost][:,:,1][1,k]
-                    push!(optadapt_temp, "Retreat-1")
-                elseif op_cost == model[:slrcost, :RetreatCost][:,:,2][1,k]
-                    push!(optadapt_temp, "Retreat-2")
-                elseif op_cost == model[:slrcost, :RetreatCost][:,:,3][1,k]
-                    push!(optadapt_temp, "Retreat-3")
-                elseif op_cost == model[:slrcost, :RetreatCost][:,:,4][1,k]
-                    push!(optadapt_temp, "Retreat-4")
-                elseif op_cost == model[:slrcost, :RetreatCost][:,:,5][1,k]
-                    push!(optadapt_temp, "Retreat-5")
-                elseif op_cost == model[:slrcost, :RetreatCost][:,:,6][1,k]
-                    push!(optadapt_temp, "Retreat-6")
-                else 
-                    push!(optadapt_temp, "None?")
+            
+            temp_opad = []
+                    
+            temp_op_2050 = []
+            temp_op_2100 = []
+            temp_na_2050 = []
+            temp_na_2100 = []
+                    
+            temp_npv = []
+            for j in range(1, num_params)
+
+                
+                if params !== nothing
+                    update_param!(model, :slrcost, :movefactor, params[1][j])
+                    update_param!(model, :slrcost, :dvbm, params[2][j])
+                    update_param!(model, :slrcost, :vslel, params[3][j])
+                    update_param!(model, :slrcost, :vslmult, params[4][j])
+                    update_param!(model, :slrcost, :wvel, params[5][j])
+                    update_param!(model, :slrcost, :wvpdl, params[6][j])
+                end
+                
+                run(model)
+            
+                optadapt_temp = []
+                for k in range(1, length(segments))
+                    op_cost = model[:slrcost, :OptimalCost][1,k]
+                    if op_cost == model[:slrcost, :ProtectCost][:,:,1][1,k]
+                        push!(optadapt_temp, "Protect-1")
+                    elseif op_cost == model[:slrcost, :ProtectCost][:,:,2][1,k]
+                        push!(optadapt_temp, "Protect-2")
+                    elseif op_cost == model[:slrcost, :ProtectCost][:,:,3][1,k]
+                        push!(optadapt_temp, "Protect-3")
+                    elseif op_cost == model[:slrcost, :ProtectCost][:,:,4][1,k]
+                        push!(optadapt_temp, "Protect-4")
+                    elseif op_cost == model[:slrcost, :ProtectCost][:,:,5][1,k]
+                        push!(optadapt_temp, "Protect-5")
+                    elseif op_cost == model[:slrcost, :RetreatCost][:,:,1][1,k]
+                        push!(optadapt_temp, "Retreat-1")
+                    elseif op_cost == model[:slrcost, :RetreatCost][:,:,2][1,k]
+                        push!(optadapt_temp, "Retreat-2")
+                    elseif op_cost == model[:slrcost, :RetreatCost][:,:,3][1,k]
+                        push!(optadapt_temp, "Retreat-3")
+                    elseif op_cost == model[:slrcost, :RetreatCost][:,:,4][1,k]
+                        push!(optadapt_temp, "Retreat-4")
+                    elseif op_cost == model[:slrcost, :RetreatCost][:,:,5][1,k]
+                        push!(optadapt_temp, "Retreat-5")
+                    elseif op_cost == model[:slrcost, :RetreatCost][:,:,6][1,k]
+                        push!(optadapt_temp, "Retreat-6")
+                    else 
+                        push!(optadapt_temp, "None?")
+                    end
+                end
+
+                if j == 1
+                    
+                    temp_opad = optadapt_temp
+
+                    temp_op_2050 = model[:slrcost, :OptimalCost][6,:]
+                    temp_op_2100 = model[:slrcost, :OptimalCost][11,:]
+                    temp_na_2050 = model[:slrcost, :NoAdaptCost][6,:]
+                    temp_na_2100 = model[:slrcost, :NoAdaptCost][11,:]
+                    
+                    temp_npv = model[:slrcost, :NPVOptimal]
+                else
+                    temp_opad = hcat(temp_opad, optadapt_temp)
+                    
+                    temp_op_2050 = hcat(temp_op_2050, model[:slrcost, :OptimalCost][6,:])
+                    temp_op_2100 = hcat(temp_op_2100, model[:slrcost, :OptimalCost][11,:])
+                    temp_na_2050 = hcat(temp_na_2050, model[:slrcost, :NoAdaptCost][6,:])
+                    temp_na_2100 = hcat(temp_na_2100, model[:slrcost, :NoAdaptCost][11,:])
+
+                    temp_npv = hcat(temp_npv,  model[:slrcost, :NPVOptimal])
+                
                 end
             end
-            
-            if i == 1
-                optimal_adapt = optadapt_temp
+            if i == 1                    
+                optimal_adapt = temp_opad
                 
-                op_array_2050 = model[:slrcost, :OptimalCost][6,:]
-                op_array_2100 = model[:slrcost, :OptimalCost][11,:]
-                na_array_2050 = model[:slrcost, :NoAdaptCost][6,:]
-                na_array_2100 = model[:slrcost, :NoAdaptCost][11,:]
+                op_array_2050 = temp_op_2050
+                op_array_2100 = temp_op_2100
+                na_array_2050 = temp_na_2050
+                na_array_2100 = temp_na_2100
                 
-                npv_array = model[:slrcost, :NPVOptimal]
+                npv_array = temp_npv
                 
             else
-                optimal_adapt = hcat(optimal_adapt, optadapt_temp)
-                
-                op_array_2050 = hcat(op_array_2050, model[:slrcost, :OptimalCost][6,:])
-                op_array_2100 = hcat(op_array_2100, model[:slrcost, :OptimalCost][11,:])
-                na_array_2050 = hcat(na_array_2050, model[:slrcost, :NoAdaptCost][6,:])
-                na_array_2100 = hcat(na_array_2100, model[:slrcost, :NoAdaptCost][11,:])
 
-                npv_array = hcat(npv_array,  model[:slrcost, :NPVOptimal])
+                optimal_adapt = hcat(optimal_adapt, temp_opad)
                 
+                op_array_2050 = hcat(op_array_2050, temp_op_2050)
+                op_array_2100 = hcat(op_array_2100, temp_op_2100)
+                na_array_2050 = hcat(na_array_2050, temp_na_2050)
+                na_array_2100 = hcat(na_array_2100, temp_na_2100)
+
+                npv_array = hcat(npv_array,  temp_npv)
             end
+            
+
         end
     
 
-    
-    CSV.write(string("OptimalCost_Gulf_2050_", ssprcp, ".csv"), DataFrame(op_array_2050, :auto))
-    CSV.write(string("OptimalCost_Gulf_2100_", ssprcp, ".csv"), DataFrame(op_array_2100, :auto))
-    CSV.write(string("NoAdaptCost_Gulf_2050_", ssprcp, ".csv"), DataFrame(na_array_2050, :auto))
-    CSV.write(string("NoAdaptCost_Gulf_2100_", ssprcp, ".csv"), DataFrame(na_array_2100, :auto))
-    CSV.write(string("NPVOptimal_Gulf_", ssprcp, ".csv"), DataFrame(npv_array, :auto))
-    CSV.write(string("OptimalStrategy_Gulf_", ssprcp, ".csv"), DataFrame(optimal_adapt, :auto))
+    if num_params !== nothing 
+        name = "Sneasy_"
+    else 
+        name = "Hermans_"
+    end
+    CSV.write(string("OptimalCost_Gulf_2050_", name, ssprcp, ".csv"), DataFrame(op_array_2050, :auto))
+    CSV.write(string("OptimalCost_Gulf_2100_", name, ssprcp, ".csv"), DataFrame(op_array_2100, :auto))
+    CSV.write(string("NoAdaptCost_Gulf_2050_", name, ssprcp, ".csv"), DataFrame(na_array_2050, :auto))
+    CSV.write(string("NoAdaptCost_Gulf_2100_", name, ssprcp, ".csv"), DataFrame(na_array_2100, :auto))
+    CSV.write(string("NPVOptimal_Gulf_", name, ssprcp, ".csv"), DataFrame(npv_array, :auto))
+    CSV.write(string("OptimalStrategy_Gulf_", name, ssprcp, ".csv"), DataFrame(optimal_adapt, :auto))
     return op_array_2050, op_array_2100, na_array_2050, na_array_2100, npv_array, optimal_adapt 
 end
---
+++ mimiCIAM_demo/sneasy_ciam/README.md
@@ -0,0 +1,19 @@
+# sneasy_param.ipynb
+
+this notebook reads slr data from Sneasy Brick, downscales segments in the US Gulf coast then runs them through 
+CIAM, creating CSV's of data output including net present value optimal, optimal cost,
+no adapt cost, and optimal adaptation strategy
+this data runs this for 10000 trials of parameters, saving parameter data
+
+*for the outputs, the rows are the segements, and the columns are typically models/trials from Sneasy's data*
+
+*column order: trial 1 param 1, trial 1 param 2, etc*
+
+## running for all segments in the gulf coast
+
+1. pull sneasy_param.ipynb, run_ciam_data_function.jl, downscaling.jl, projections_csv files (for contribution data), gulf_segments.csv, lonlat_gulf_list.csv, move_factor_s.csv, dvbm_s.csv, vslel_s.csv, vslmult_s.csv, wvel_s.csv, wvpdl_s.csv
+2. run cells 1-9 to read necessray data (also make sure you have packages installed!)
+*do NOT run cell 15!!!!! else you change the parameter data!!!!*
+3. run cells 17-18 to get parameters
+4. run cells 24-29 
+(should end up with 24 csvs)
